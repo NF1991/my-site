d910729714cec2d41d770bb7af41df4ba3da43b2
NF1991 <104265818+NF1991@users.noreply.github.com>
2023-02-27 18:30:08 +0000

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import htmlImg from "/../public/assets/skills/html-5.png";
+import cssImg from "/../public/assets/skills/css-3.png";
+import jsImg from "/../public/assets/skills/js.png";
+import tailwindImg from "/../public/assets/skills/tailwind-css-icon.png";
+import reactImg from "/../public/assets/skills/react.png";
+import nextImg from "/../public/assets/skills/nextjs-icon.png";
+import bootstrapImg from "/../public/assets/skills/bootstrap.png";
+import figmaImg from "/../public/assets/skills/figma.png";
+import xdImg from "/../public/assets/skills/xd.png";
+import illustratorImg from "/../public/assets/skills/illustrator.png";
 
 const Skills = () => {
   return (
@@ -9,111 +19,61 @@ const Skills = () => {
         <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-8">
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/html-5.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={htmlImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/css-3.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={cssImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/js.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={jsImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/tailwind-css-icon.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={tailwindImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/react.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={reactImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/nextjs-icon.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={nextImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/bootstrap.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={bootstrapImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/figma.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={figmaImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/xd.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={xdImg} alt="/" width="200" height="200" />
             </div>
           </div>
 
           <div className="shadow-md p-6 hover:scale-105 ease-in duration-300">
             <div className="m-auto">
-              <Image
-                src="/../public/assets/skills/illustrator.png"
-                alt="/"
-                width="200"
-                height="200"
-              />
+              <Image src={illustratorImg} alt="/" width="200" height="200" />
             </div>
           </div>
         </div>

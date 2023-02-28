be7f4fcf5da8562ed1a6ab7cae83bc076e3aa2a7
NF1991 <104265818+NF1991@users.noreply.github.com>
2023-02-28 08:58:32 +0000

    Update success

--- pages/success.jsx
+++ pages/success.jsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export default function Success() {
   return (
     <div className="bg-[#f5f5f5] h-[100vh] flex items-center justify-center flex-col">
       <p className="text-lg text-black">Form successfully submitted!</p>
       <div className="pt-2">
-        <a className="uppercase text-black underline" href="/">
+        <Link className="uppercase text-black underline" href="/">
+          {" "}
           Return to Homepage
-        </a>
+        </Link>
       </div>
     </div>
   );

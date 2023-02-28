b3cd0d1ecf2db0ae502be6cc97f7be4e07e25f89
NF1991 <104265818+NF1991@users.noreply.github.com>
2023-02-28 08:56:04 +0000

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -1,7 +1,12 @@
 export default function Success() {
   return (
-    <div className="flex items-center justify-center">
-      <p className="text-lg">Form successfully submitted!</p>
+    <div className="bg-[#f5f5f5] h-[100vh] flex items-center justify-center flex-col">
+      <p className="text-lg text-black">Form successfully submitted!</p>
+      <div className="pt-2">
+        <a className="uppercase text-black underline" href="/">
+          Return to Homepage
+        </a>
+      </div>
     </div>
   );
 }
